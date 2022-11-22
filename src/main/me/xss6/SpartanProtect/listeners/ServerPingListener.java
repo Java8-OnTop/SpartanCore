0a7f85f2e025945d24aba72a365a4df05fdd3cee
XSS6 <72362725+Java8-OnTop@users.noreply.github.com>
2022-11-22 10:17:04 -0700

    Update ServerPingListener.java

--- src/main/me/xss6/SpartanProtect/listeners/ServerPingListener.java
+++ src/main/me/xss6/SpartanProtect/listeners/ServerPingListener.java
@@ -3,7 +3,6 @@ package me.xss6.SpartanProtect.listeners;
 import org.bukkit.event.EventHandler;
 import java.util.List;
 import org.bukkit.ChatColor;
-import me.xss6.SpartanProtect.motd.RandomMOTD;
 import org.bukkit.Bukkit;
 import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.event.Listener;
@@ -13,11 +12,11 @@ public class ServerPingListener implements Listener
     @EventHandler
     public void a(final ServerListPingEvent event) {
         if (Bukkit.getServer().hasWhitelist()) {
-            final List<String> whitelistList = (List<String>)RandomMOTD.Instance.getConfig().getStringList("Settings.WhitelistMOTDS");
+            final List<String> whitelistList = (List<String>)RandomMOTD.Instance.getConfig().getStringList("motds");
             event.setMotd(ChatColor.translateAlternateColorCodes('&', (String)whitelistList.get((int)Math.floor(Math.random() * whitelistList.size()))));
         }
         else {
-            final List<String> list = (List<String>)RandomMOTD.Instance.getConfig().getStringList("Settings.MOTDS");
+            final List<String> list = (List<String>)RandomMOTD.Instance.getConfig().getStringList("motds");
             event.setMotd(ChatColor.translateAlternateColorCodes('&', (String)list.get((int)Math.floor(Math.random() * list.size()))));
         }
     }
