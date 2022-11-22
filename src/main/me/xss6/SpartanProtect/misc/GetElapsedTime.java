d587cd16cf7498d377e46424e0bfe6f9403f89bd
XSS6 <72362725+Java8-OnTop@users.noreply.github.com>
2022-11-21 22:52:29 -0700

    Create GetElapsedTime.java

---
+++ src/main/me/xss6/SpartanProtect/misc/GetElapsedTime.java
@@ -0,0 +1,33 @@
+package me.xss6.SpartanProtect.misc;
+
+public class GetElapsedTime {
+    
+    public static String get(long timeToConvert) {
+         // UPTIME
+        long different = System.currentTimeMillis() - timeToConvert;
+                          
+        long secondsInMilli = 1000;
+        long minutesInMilli = secondsInMilli * 60;
+        long hoursInMilli = minutesInMilli * 60;
+        long daysInMilli = hoursInMilli * 24;
+        long weeksInMilli = daysInMilli * 7;
+                          
+        long elapsedWeeks = different / weeksInMilli;
+        different = different % weeksInMilli;
+                          
+         long elapsedDays = different / daysInMilli;
+         different = different % daysInMilli;
+
+         long elapsedHours = different / hoursInMilli;
+         different = different % hoursInMilli;
+
+         long elapsedMinutes = different / minutesInMilli;
+         different = different % minutesInMilli;
+
+         long elapsedSeconds = different / secondsInMilli;
+         
+         return String.format("§d%d w§b - §d%d d§b - §d%d h§b - §d%d m§b - §d%d s", elapsedWeeks,elapsedDays,elapsedHours,elapsedMinutes,elapsedSeconds);
+
+    }
+    
+}
