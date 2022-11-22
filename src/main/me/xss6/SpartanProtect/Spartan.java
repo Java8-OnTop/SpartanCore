7d82ffed48bbd7942058d73937737d48931642d7
XSS6 <72362725+Java8-OnTop@users.noreply.github.com>
2022-11-21 21:10:44 -0700

    Update Spartan.java

--- src/main/me/xss6/SpartanProtect/Spartan.java
+++ src/main/me/xss6/SpartanProtect/Spartan.java
@@ -16,21 +16,30 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import me.xss6.SpartanProtect.listeners.ServerPingListener;
 
 public final class Spartan extends JavaPlugin implements Listener, CommandExecutor {
     @Override
+    final Spartan Instance;
     public void onEnable() {
         // Plugin startup logic
         getLogger().info("Spartan Protection is enabled");
         checkConfig();
+        this.saveDefaultConfig();
         getServer().getPluginManager().registerEvents(this, this);
         this.getCommand("SpartanProtect").setExecutor(this); //TODO: handle NullPointerException thing (what should I do?)
+        Spartan.Instance = this;
+        this.getServer().getPluginManager().registerEvents((Listener)new ServerPingListener(), (Plugin)this);
+        this.getLogger().info("SpartanProtect v" + "0.0.2" + " Enabled!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
         getLogger().info("Spartan Protection is disabled");
+        this.getLogger().info("RandomMOTD v" + this.getDescription().getVersion() + " Disabled!");
     }
 
     @Override
