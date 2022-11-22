c34e1384661d4ad0cd497dc60cf34b356149fd87
XSS6 <72362725+Java8-OnTop@users.noreply.github.com>
2022-11-21 21:31:27 -0700

    Create Utils.java

---
+++ src/main/me/xss6/SpartanProtect/utils/Utils.java
@@ -0,0 +1,9 @@
+package me.xss6.SpartanProtect.utils;
+
+import org.bukkit.ChatColor;
+
+public class Utils {
+    public static String chat(String s) {
+        return ChatColor.translateAlternateColorCodes('&', s);
+    }
+}
