3f9e2b4209f8a90db35dcededf46b0913fa9945b
XSS6 <72362725+Java8-OnTop@users.noreply.github.com>
2022-11-21 21:01:10 -0700

    Create ServerPingListener.java

---
+++ src/main/me/xss6/SpartanProtect/listeners/ServerPingListener.java
@@ -0,0 +1,24 @@
+package me.xss6.SpartanProtect.listeners;
+
+import org.bukkit.event.EventHandler;
+import java.util.List;
+import org.bukkit.ChatColor;
+import me.xss6.SpartanProtect.motd.RandomMOTD;
+import org.bukkit.Bukkit;
+import org.bukkit.event.server.ServerListPingEvent;
+import org.bukkit.event.Listener;
+
+public class ServerPingListener implements Listener
+{
+    @EventHandler
+    public void a(final ServerListPingEvent event) {
+        if (Bukkit.getServer().hasWhitelist()) {
+            final List<String> whitelistList = (List<String>)RandomMOTD.Instance.getConfig().getStringList("Settings.WhitelistMOTDS");
+            event.setMotd(ChatColor.translateAlternateColorCodes('&', (String)whitelistList.get((int)Math.floor(Math.random() * whitelistList.size()))));
+        }
+        else {
+            final List<String> list = (List<String>)RandomMOTD.Instance.getConfig().getStringList("Settings.MOTDS");
+            event.setMotd(ChatColor.translateAlternateColorCodes('&', (String)list.get((int)Math.floor(Math.random() * list.size()))));
+        }
+    }
+}
